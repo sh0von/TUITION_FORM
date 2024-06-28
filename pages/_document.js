9e19ea68524b84ecd50b1d7524fd3c86af826b4f
sh0von <minar.svn@gmail.com>
2024-06-28 11:59:40 +0600

    feat: Add lazy loading for images and update analytics tracking

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react"
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+      <title>Tuition Survey Form</title>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   );
