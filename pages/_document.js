d62468d77cc4dc2c70ebdcb3dfb3e04ff8e9a240
sh0von <minar.svn@gmail.com>
2024-06-28 12:16:23 +0600

    feat: Add meta tags for social media sharing in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Tuition Survey Form</title>
+        <meta name="description" content="Survey form for tuition services in Bangladesh" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://tse4.mm.bing.net/th?id=OIG3.1fgFuX71zIVYjIQg_BhN&pid=ImgGn" />
+        <meta property="og:title" content="Tuition Survey Form" />
+        <meta property="og:description" content="Survey form for tuition services in Bangladesh" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content="https://tse4.mm.bing.net/th?id=OIG3.1fgFuX71zIVYjIQg_BhN&pid=ImgGn" />
+        <meta property="twitter:title" content="Tuition Survey Form" />
+        <meta property="twitter:description" content="Survey form for tuition services in Bangladesh" />
+      </Head>
       <body>
-      <title>Tuition Survey Form</title>
         <Main />
         <NextScript />
       </body>
