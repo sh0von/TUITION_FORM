238be33d49b91ec43a7e834e3b54ab360eeabff3
sh0von <minar.svn@gmail.com>
2024-06-28 14:58:53 +0600

    chore: Update MongoDB database name in form.js

--- pages/api/form.js
+++ pages/api/form.js
@@ -3,11 +3,11 @@ import clientPromise from '../../lib/mongodb';
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const client = await clientPromise;
-    const db = client.db('1');
+    const db = client.db('tuition_form');
 
     const { formData } = req.body;
 
-    const result = await db.collection('2').insertOne(formData);
+    const result = await db.collection('tuition_form').insertOne(formData);
 
     res.status(200).json({ success: true, result });
   } else {
